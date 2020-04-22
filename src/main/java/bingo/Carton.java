36735fb7f2e4e6ef8b61b4e6a2f94f40fa018248
Gabriel <gabionezero@gmail.com>
2020-04-22 20:44:26 +0200

    Añade comentarios para los métodos de la clase Cartón

--- src/main/java/bingo/Carton.java
+++ src/main/java/bingo/Carton.java
@@ -23,14 +23,13 @@ public class Carton {
         generarCarton();
     }
 
-    //En este método se generarán los números aleatoriamente para el cartón en cuestión, 
-    //siguiendo las directrices establecidas en cuanto a la colocación de los números
-    //Es un método "private" ya que solo querremos tener acceso a él desde la propia clase
-    //para usarlo en el constructor
+    //Método principal de la clase
+    //1ºGenera los números, luego establece los huecos correspondientes 
+    //y por último comprueba que todo es correcto, si no es así se vuelve a repetir
+    //el proceso hasta que se genera un cartón que tenga todas las caracteristicas
+    //que debe.
     private void generarCarton() {
 
-//        generarNumeros();
-//        generarHuecos();
         do {
             generarNumeros();
             generarHuecos();
@@ -39,6 +38,11 @@ public class Carton {
 
     }
 
+    //1º GENERAR NÚMEROS
+    //En este método se generarán los números aleatoriamente para el cartón en cuestión, 
+    //siguiendo las directrices establecidas en cuanto a la colocación de los números
+    //Es un método "private" ya que solo querremos tener acceso a él desde la propia clase
+    //para usarlo en el constructor
     private void generarNumeros() {
         Random varRandom = new Random();
 
@@ -64,7 +68,7 @@ public class Carton {
                             nuevoNumero = varRandom.nextInt(9) + 1;
                         } while (nuevoNumero <= filaAnterior[j]);
                         filaAnterior[j] = nuevoNumero; //Actualizamos los número de la fila anterior
-                        this.numerosCarton[i][j] = nuevoNumero; //Puesto que es correcto establecemos el número en la posición del cartón correspondiente
+                        this.numerosCarton[i][j] = nuevoNumero; //Puesto que es correcto, establecemos el número en la posición del cartón correspondiente
                         break;
 
                     case 8: //9ª columna
@@ -107,32 +111,39 @@ public class Carton {
         }
     }
 
+    //Método que añade los huecos restantes que corresponden 
+    //(4 huecos por cada fila en orden aleatorio, teniendo en cuenta los huecos
+    //que ya hayan podido generarse al colocar los números)
     private void generarHuecos() {
         Random varRandom = new Random();
 
-        ArrayList<Integer> posicionesHuecos = new ArrayList<>();
+        //Creamos una lista con las posiciones que tendrán los huecos para una fila en cuestión
+        ArrayList<Integer> posicionesDeLosHuecos = new ArrayList<>();
 
-        for (int i = 0; i < this.numerosCarton.length; i++) {
+        for (int i = 0; i < this.numerosCarton.length; i++) { //Recorremos las 3 FILAS
 
-            posicionesHuecos.removeAll(posicionesHuecos);
+            posicionesDeLosHuecos.removeAll(posicionesDeLosHuecos); //Aseguramos que ña lsita de posiciones esté vacío antes de comenzar a revisar cada fila
             do {
-
+                //Rellenamos la lista con valores aleatorios correspondientes a las posiciones posibles de la fila
                 int huecoRandom = varRandom.nextInt(9);
-                if (!posicionesHuecos.contains(huecoRandom)) {
-                    posicionesHuecos.add(huecoRandom);
+                if (!posicionesDeLosHuecos.contains(huecoRandom)) {//Si el número ya existe en la lista, lo descartamos
+                    posicionesDeLosHuecos.add(huecoRandom);
                 }
-            } while (posicionesHuecos.size() < 4);
-            int huecos = contarHuecos(i);
+            } while (posicionesDeLosHuecos.size() < 4);//Seguimos sacando numeros aleatorios para la lista hasta tener 4 posiciones
+            int huecos = contarHuecos(i); //Establecemos eñl contador de huecos y contamos el número de huecos que ya existian anteriormente para esta línea, para tenerlos en cuenta para el total de 4 huecos por línea
 
+            //Ahora que ya tenemos las 4 posiciones aleatorias para ESTA FILA, recorremos las 9 posiciones
             for (int j = 0; j < this.numerosCarton[0].length; j++) {
 
-                if (this.numerosCarton[i][j] != 0) {
-                    if (posicionesHuecos.contains(j)) {
-                        this.numerosCarton[i][j] = 0;
-                        huecos++;
-                    }
+                //Si en la posición actual no hay ya un hueco (0) y coincide la posición con una de la lista,
+                //establecemos un hueco (0)
+                //y sumamos 1 al contador de huecos por linea
+                if (this.numerosCarton[i][j] != 0 && posicionesDeLosHuecos.contains(j)) {
+                    this.numerosCarton[i][j] = 0;
+                    huecos++;
                 }
-
+                //En caso de que ya hayamos alcanzado los 4 huecos en esta línea
+                //salimos de inmediato del bucle
                 if (huecos >= 4) {
                     break;
                 }
@@ -142,9 +153,13 @@ public class Carton {
         }
     }
 
+    //Cuenta los huecos que hay en una línea
+    //@param - fila en la que nos encontramos actualmente
+    //@return - número de huecos que existen en la línea actual
     private int contarHuecos(int fila) {
         int huecos = 0;
 
+        //Con un bucle contamos los 0 para saber cuántos huecos hay
         for (int i = 0; i < this.numerosCarton[0].length; i++) {
             if (this.numerosCarton[fila][i] == 0) {
                 huecos++;
@@ -153,42 +168,55 @@ public class Carton {
         return huecos;
     }
 
+    //Comprueba que todos los huecos estén colocados debidamente, es decir,
+    //que además de que haya 4 huecos por línea, no quede ninguna columna vacía
+    // o ninguna columna con tres números
+    //@return - si el cartón es correcto o no
     private boolean comprobarCarton() {
-        int huecosPorFila = 0;
-        int huecosPorColumna = 0;
-        boolean huecosCorrectos = true;
+        int huecosPorFila;
+        int huecosPorColumna;
 
+        
+        //Comprobamos primero todo el cartón por filas
         for (int i = 0; i < this.numerosCarton.length; i++) {
-
-            huecosPorFila = 0;
+            
+            huecosPorFila = 0;//resetamos el contador para cada fila
             for (int j = 0; j < this.numerosCarton[0].length; j++) {
-
+                //Añadimos uno al contador por cada  hueco
                 if (this.numerosCarton[i][j] == 0) {
                     huecosPorFila++;
                 }
 
             }
+            //Si resulta que no hay 4 huecos por cada linea devolvemos false y
+            //el cartón no se da por válido
             if (huecosPorFila != 4) {
                 return false;
             }
         }
 
+        //Comprobamos todo el cartón por columnas
         for (int i = 0; i < this.numerosCarton[0].length; i++) {
-            huecosPorColumna = 0;
+            
+            huecosPorColumna = 0; //reseteamos el contador en cada columna
             for (int j = 0; j < this.numerosCarton.length; j++) {
 
+                //En caso de haber un hueco (0) sumamos 1 al contador
                 if (this.numerosCarton[j][i] == 0) {
                     huecosPorColumna++;
                 }
 
             }
-             if (huecosPorColumna != 1 && huecosPorColumna != 2) {
-            return false;
-        }
-       
-        }
+            //En caso de que en la columna no haya huecos o haya más de 2
+            //se dará por mal el cartón y por tanto devolveremos false
+            if (huecosPorColumna != 1 && huecosPorColumna != 2) {
+                return false;
+            }
 
-        return huecosCorrectos;
+        }
+        //Si ha llegado hasta aquí la ejecución es que todos los huecos son correctos
+        //por tanto daremos por válido el cartón y devolvemos true
+        return true;
     }
 //Imrpimimos los números guardados en el cartón
 //y le damos formato
