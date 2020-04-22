9511f43a4e5b43112b939598af6c3106766ba53a
Gabriel <gabionezero@gmail.com>
2020-04-22 21:44:20 +0200

    Añade la lógica de cada "turno" en la clase Main

--- src/main/java/bingo/JuegoBingo.java
+++ src/main/java/bingo/JuegoBingo.java
@@ -12,6 +12,56 @@ package bingo;
 public class JuegoBingo {
 
     public static void main(String[] args) {
+        
+        Bombo bombo = new Bombo();
+        Carton carton = new Carton();
+        
+        int bolaSacada;
+        
+        System.out.println("Este es su cartón:");
+       
+        carton.imprimirCarton();
+        
+        do{
+            bolaSacada = bombo.sacarBola();
+            System.out.println("Ha salido el número: " + bolaSacada);
+            System.out.println("-------------");            
+            
+            if(carton.tacharCasilla(bolaSacada)){
+                System.out.println("Se ha tachado el número " + bolaSacada);
+                
+               if(carton.comprobarSiLinea(0)){
+                   System.out.println("LINEA - en la línea: " + 1);
+                   
+                   if(carton.comprobarSiBingo()){
+                       System.out.println("BINGO!!!!!!!!!!!");
+                       System.out.println("ENHORABUENA!!!!!");
+                       break;
+                   }
+               }
+               if(carton.comprobarSiLinea(1)){
+                   System.out.println("LINEA - en la línea: " + 2);
+                    if(carton.comprobarSiBingo()){
+                       System.out.println("BINGO!!!!!!!!!!!");
+                       System.out.println("ENHORABUENA!!!!!");
+                       break;
+                   }
+               }
+               if(carton.comprobarSiLinea(2)){
+                   System.out.println("LINEA - en la línea: " + 3);
+                    if(carton.comprobarSiBingo()){
+                       System.out.println("BINGO!!!!!!!!!!!");
+                       System.out.println("ENHORABUENA!!!!!");
+                       break;
+                   }
+               }
+            }else{
+                System.out.println("No hubo suerte con esta bola");
+            }
+            
+            carton.imprimirCarton();
+            
+        }while(!carton.comprobarSiBingo());
     }
     
 }
