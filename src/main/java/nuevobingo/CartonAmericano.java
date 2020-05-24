6aabb978bb4ac0f32d5308a1a2828ceac5339728
Gabriel <gabionezero@gmail.com>
2020-05-24 22:42:50 +0200

    Añade el método generarCarton y rellenarMatrriz para la clase CartonAmericano

--- src/main/java/nuevobingo/CartonAmericano.java
+++ src/main/java/nuevobingo/CartonAmericano.java
@@ -32,50 +32,74 @@ public final class CartonAmericano extends Carton {
     //=======================MÉTODOS=================================
     @Override
     public void generarCarton() {
+        int[] listaNumeros = new int[5];
 
-        int nuevoNumero;
-        int[] columna = new int[5];
+        for (int i = 0; i < COLUMNAS; i++) {
 
-        for (int i = 0; i < this.getMatriz().length; i++) {
-
-            for (int j = 0; j < this.getMatriz()[i].length; j++) {
-
-                switch (j) {
-                    case 0:
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        break;
-                }
+            switch (i) {
+                case 0:
+                    listaNumeros = generarColumna(1, 15);
+                    rellenarMatriz(i, listaNumeros);
+                    break;
+                case 1:
+                    listaNumeros = generarColumna(16, 30);
+                    rellenarMatriz(i, listaNumeros);
+                    break;
+                case 2:
+                    listaNumeros = generarColumna(31, 45);
+                    rellenarMatriz(i, listaNumeros);
+                    break;
+                case 3:
+                    listaNumeros = generarColumna(46, 60);
+                    rellenarMatriz(i, listaNumeros);
+                    break;
+                default:
+                    listaNumeros = generarColumna(61, 75);
+                    rellenarMatriz(i, listaNumeros);
+                    break;
             }
 
         }
 
+        this.getMatriz()[2][2] = 0;
+
     }
 
-    public ArrayList<Integer> generarColumna(int minimo, int maximo) {
+    public int[] generarColumna(int minimo, int maximo) {
         Random varRandom = new Random();
-        ArrayList<Integer> columna = new ArrayList<>();
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+        int[] columna = new int[COLUMNAS];
         int numero;
 
         for (int i = 0; i < COLUMNAS; i++) {
 
             do {
                 numero = varRandom.ints(minimo, 0, maximo).findFirst().getAsInt();
-            } while (columna.contains(numero));
-            
-            columna.add(numero);
+            } while (listaNumeros.contains(numero));
+
+            listaNumeros.add(numero);
 
         }
-        
-        Collections.sort(columna);
+
+        Collections.sort(listaNumeros);
+
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            columna[i] = listaNumeros.get(i);
+        }
 
         return columna;
+    }
+
+    public void rellenarMatriz(int columna, int[] numeros) {
+
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+
+                this.getMatriz()[i][columna] = numeros[i];
+
+            }
+        }
+
     }
 
     //==================GETTERS & SETTERS============================
@@ -84,9 +108,5 @@ public final class CartonAmericano extends Carton {
     }
     //======================TO STRING================================
 
-    @Override
-    public String toString() {
-        return "CartonAmericano{" + "premio=" + premio + '}';
-    }
-
+   
 }
