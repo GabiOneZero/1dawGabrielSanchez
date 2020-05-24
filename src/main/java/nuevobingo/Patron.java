07fa498cc96a22c636eaba393a03b85f096f29b9
Gabriel <gabionezero@gmail.com>
2020-05-24 20:07:22 +0200

    Crea los patrones de puntos para el Enum

--- src/main/java/nuevobingo/Patron.java
+++ src/main/java/nuevobingo/Patron.java
@@ -6,37 +6,156 @@
 package nuevobingo;
 
 import java.awt.Point;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author Gabriel
  */
 public enum Patron {
-    
-    CARTON_LLENO(),
-    FORMA_X(),
-    FORMA_Y();
-    
-    private List<Point> casillas;
+
+    CARTON_LLENO(cartonLleno(), "Cartón con números en todas las casillas."),
+    MARCO(marco(), "Cartón con números solo en el borde exterior del cartón"),
+    FORMA_CRUZ(cruz(), "Cartón con números formando una +"),
+    FORMA_X(cartonX(), "Cartón con números formando una X."),
+    FORMA_Y(cartonY(), "Cartón con números formando una Y.");
+
+    private ArrayList<Point> casillas;
     private String descripcion;
-    
-    Patron(){
-        
-    }
-    
-    Patron(List<Point>casillas, String descripcion){
+
+    Patron(ArrayList<Point> casillas, String descripcion) {
         this.casillas = casillas;
         this.descripcion = descripcion;
     }
 
-    public List<Point> getCasillas() {
+    private static ArrayList<Point> cartonLleno() {
+        ArrayList<Point> listaPuntos = new ArrayList<>();
+
+        listaPuntos.add(new Point(0, 0));
+        listaPuntos.add(new Point(0, 1));
+        listaPuntos.add(new Point(0, 2));
+        listaPuntos.add(new Point(0, 3));
+        listaPuntos.add(new Point(0, 4));
+
+        listaPuntos.add(new Point(1, 0));
+        listaPuntos.add(new Point(1, 1));
+        listaPuntos.add(new Point(1, 2));
+        listaPuntos.add(new Point(1, 3));
+        listaPuntos.add(new Point(1, 4));
+
+        listaPuntos.add(new Point(2, 0));
+        listaPuntos.add(new Point(2, 1));
+        listaPuntos.add(new Point(2, 2));
+        listaPuntos.add(new Point(2, 3));
+        listaPuntos.add(new Point(2, 4));
+
+        listaPuntos.add(new Point(3, 0));
+        listaPuntos.add(new Point(3, 1));
+        listaPuntos.add(new Point(3, 2));
+        listaPuntos.add(new Point(3, 3));
+        listaPuntos.add(new Point(3, 4));
+
+        listaPuntos.add(new Point(4, 0));
+        listaPuntos.add(new Point(4, 1));
+        listaPuntos.add(new Point(4, 2));
+        listaPuntos.add(new Point(4, 3));
+        listaPuntos.add(new Point(4, 4));
+
+        return listaPuntos;
+    }
+
+    private static ArrayList<Point> marco() {
+        ArrayList<Point> listaPuntos = new ArrayList<>();
+
+        listaPuntos.add(new Point(0, 0));
+        listaPuntos.add(new Point(0, 1));
+        listaPuntos.add(new Point(0, 2));
+        listaPuntos.add(new Point(0, 3));
+        listaPuntos.add(new Point(0, 4));
+
+        listaPuntos.add(new Point(1, 0));
+        listaPuntos.add(new Point(1, 4));
+
+        listaPuntos.add(new Point(2, 0));
+        listaPuntos.add(new Point(2, 4));
+
+        listaPuntos.add(new Point(3, 0));
+        listaPuntos.add(new Point(3, 4));
+
+        listaPuntos.add(new Point(4, 0));
+        listaPuntos.add(new Point(4, 1));
+        listaPuntos.add(new Point(4, 2));
+        listaPuntos.add(new Point(4, 3));
+        listaPuntos.add(new Point(4, 4));
+
+        return listaPuntos;
+    }
+
+    private static ArrayList<Point> cruz() {
+        ArrayList<Point> listaPuntos = new ArrayList<>();
+
+        listaPuntos.add(new Point(0, 2));
+
+        listaPuntos.add(new Point(1, 2));
+
+        listaPuntos.add(new Point(2, 0));
+        listaPuntos.add(new Point(2, 1));
+        listaPuntos.add(new Point(2, 2));
+        listaPuntos.add(new Point(2, 3));
+        listaPuntos.add(new Point(2, 4));
+
+        listaPuntos.add(new Point(3, 2));
+
+        listaPuntos.add(new Point(4, 2));
+
+        return listaPuntos;
+    }
+
+    private static ArrayList<Point> cartonX() {
+        ArrayList<Point> listaPuntos = new ArrayList<>();
+
+        listaPuntos.add(new Point(0, 0));
+        listaPuntos.add(new Point(0, 4));
+
+        listaPuntos.add(new Point(1, 1));     
+        listaPuntos.add(new Point(1, 3));  
+
+        listaPuntos.add(new Point(2, 2));
+        
+   
+        listaPuntos.add(new Point(3, 1));   
+        listaPuntos.add(new Point(3, 3));
+
+        listaPuntos.add(new Point(4, 0));
+        listaPuntos.add(new Point(4, 4));
+
+        return listaPuntos;
+    }
+    
+      private static ArrayList<Point> cartonY(){
+        ArrayList<Point> listaPuntos = new ArrayList<>();
+        
+        listaPuntos.add(new Point (0, 0));
+        listaPuntos.add(new Point (0, 4));        
+    
+        listaPuntos.add(new Point (1, 1));
+        listaPuntos.add(new Point (1, 3));
+        
+        listaPuntos.add(new Point (2, 2));
+        
+        listaPuntos.add(new Point (3, 2));
+        
+        listaPuntos.add(new Point (4, 2));
+       
+        return listaPuntos;
+    }
+
+    public ArrayList<Point> getCasillas() {
         return casillas;
     }
 
     public String getDescripcion() {
         return descripcion;
     }
-    
-    
+
 }
